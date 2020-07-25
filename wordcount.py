4aad4a77fbc0567369f187d11915d93df1923142
Caeli Field <caeli.field@gmail.com>
2020-07-25 23:42:51 +0000

    Wrote initial attempt at word count function
    Co-authored-by: Ryann Rourk <ryannrourk@gmail.com>

---
+++ wordcount.py
@@ -0,0 +1,25 @@
+# put your code here.
+#pseudocode
+#def 
+    #open file
+    #create empty dictionary
+    #for line in file
+        #r strip
+        #tokenize by space
+        #add each new word to dict / update existing value + 1
+
+    #return dictionary key value pairs
+
+#call function & print
+
+def word_count(filename):
+    word_dict = {}
+    with open(filename) as f:
+        for line in f:
+            line = line.rstrip()
+            words = line.split(" ")
+            for word in words:
+                word_dict[word] = word_dict.get(word, 0) + 1
+    return word_dict
+
+print(word_count("test.txt"))
